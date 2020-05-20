096869e6a780f9bf4a381eba3993e18d76f98e62
AditMeh <aditmeh123@gmail.com>
2020-05-20 02:49:57 -0400

    Added annotation script and konva

---
+++ centra-label/src/components/Annotation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueKonva from 'vue-konva'
+Vue.use(VueKonva)
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
